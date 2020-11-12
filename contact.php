0e2cd481204e322d567dbf93bf7492fa2e9521f9
Pann Ei Phyu <panneiphyu5cs43@gmail.com>
2020-11-12 12:46:31 +0630

    first commit

---
+++ contact.php
@@ -0,0 +1,79 @@
+<section id="contact">
+    <footer class="bg-light">
+      <div class="container-fluid py-3 footer">
+        <div class="row ml-5">
+          <div class="col-md-5 mt-3">
+              <h5 class="mb-3 text-white font-dosis mr-2"><strong>CONTACT US </strong></h5>
+              <div class="row">
+                <div class="col-1">
+                    <i class="fa fa-home text-white-50"></i>
+                </div>
+                <div class="col-11">
+                  <p class="text-white-50 text-left font-dosis">No.157,Bogyoke Road,Ywar Bae East Ward,Near CB Bank(2),Pyay,Bago Region,Myanmar.08156</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-1">
+                    <i class="fa fa-phone text-white-50"></i>
+                </div>
+                <div class="col-11">
+                  <p class="text-white-50 text-left">09 893 19994</p>
+               </div>
+              </div>
+
+              <div class="row">
+               <div class="col-1">
+                    <i class="fa fa-envelope text-white-50"></i>
+                </div>
+                <div class="col-11">
+                  <a href="#"><p class="text-white-50 text-left">hsp.marketingagency@gmail.com</p></a>
+               </div>
+              </div>
+
+              <div class="row">
+                
+                <div class="col-1">
+                    <i class="far fa-credit-card text-white-50"></i>
+                </div>
+                <div class="col-11">
+                  <p class="text-white-50 text-left font-dosis">Facebook Marketing Service, Messager Marketing Service, Logo Design Service</p>
+                </div>
+              </div>
+          </div>
+          <div class="col-md-3 mt-3">
+            <h5 class="mb-3 text-white font-dosis"><strong>SUPPORT</strong></h5>
+            <p class="text-white-50 font-dosis">Customer Support</p>
+            <p class="text-white-50 font-dosis mb-5">Service Policies</p>
+             <h5 class="mb-3 text-white font-dosis"><strong>FOLLOW US</strong></h5>
+             <a href="https://www.facebook.com/hspdigitalmarketingagency/about/?ref=page_internal">
+                <i class="fab fa-facebook-f text-white-50"></i>
+              </a>
+             
+             <a href="https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1KknUe0qoKPJWFL-hpWMm4CYLEFg&ll=18.821413334887794%2C95.2319207725624&z=16">
+              <!-- <i class="fa fa-map-marker text-white-50 ml-4 mb-3"></i> -->
+             </a>
+          </div>
+          <div class="col-md-4 mt-3">
+            <h5 class="text-white font-dosis mb-3">CONTACT LOCATION</h5>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1888.2550578390835!2d95.2154626153084!3d18.819972967354005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c7835073d5366d%3A0x85271bdcd32f269a!2sBogyoke%20Road%2C%20Pye!5e0!3m2!1sen!2smm!4v1605159741168!5m2!1sen!2smm" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <!-- <h5 class="text-white font-dosis">SUBSCRIBE US</h5>
+            <div class="row">
+                <p class="text-white-50 ml-3">Subscribe of our newletters.</p>
+            </div>
+            <form>
+            <div class="form-row">
+                <div class="col-7">
+                  <input type="email" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            </form> 
+            <div class="row mt-3 ml-2">
+                <button class="btn-sm btn-danger bg-red text-white">SUBSCRIBE</button>
+            </div> -->
+          </div>
+        
+        </div>
+      </div>
+      </footer>
+  </section>
